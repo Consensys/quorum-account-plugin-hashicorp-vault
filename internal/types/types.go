ee634637dfe20e28481c3d9eeb6fb82e5647a525
chris-j-h <chrishounsom@icloud.com>
2020-06-08 09:16:01 +0100

    Represent ethereum addresses with new type

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,12 +1,37 @@
 package types
 
 import (
+	"encoding/hex"
+	"fmt"
 	"net/url"
-
-	"github.com/ethereum/go-ethereum/common"
+	"strings"
 )
 
 type Account struct {
-	Address common.Address
+	Address Address
 	URL     *url.URL
+}
+
+const addressLength = 20
+
+type Address [addressLength]byte
+
+func NewAddress(byt []byte) (Address, error) {
+	if len(byt) != addressLength {
+		return Address{}, fmt.Errorf("account address must have length %v bytes", addressLength)
+	}
+	var addr Address
+	copy(addr[:], byt)
+
+	return addr, nil
+}
+
+// NewAddressFromHex creates a new Address from the provided hex string-representation.  The hexAddr can be with/without the '0x' prefix.
+func NewAddressFromHex(addr string) (Address, error) {
+	addr = strings.TrimPrefix(addr, "0x")
+	byt, err := hex.DecodeString(addr)
+	if err != nil {
+		return Address{}, fmt.Errorf("invalid hex address: %v", err)
+	}
+	return NewAddress(byt)
 }
